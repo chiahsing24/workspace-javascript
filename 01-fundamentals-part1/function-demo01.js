6947e279e7f906173bb34c4bf817154c50b88fd8
chiahsingchu <chiahsingtony@gmail.com>
2023-10-21 10:05:23 -0700

    Add function example

--- 01-fundamentals-part1/function-demo01.js
+++ 01-fundamentals-part1/function-demo01.js
@@ -1,4 +1,5 @@
 // Function declaration
+// Function that can be used before it's declared
 function calcAge1(birthYear) {
   return 2023 - birthYear;
 }
@@ -6,6 +7,7 @@ function calcAge1(birthYear) {
 const age1 = calcAge1(1990);
 
 // Function expression
+// Essentially a function value stored in a variable
 const calcAge2 = function (birthYear) {
   return 2023 - birthYear;
 };
@@ -13,3 +15,9 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1992);
 
 console.log(age1, age2);
+
+// Arrow function
+// Great for a quick one-line function, has no "this" keyword
+const calcAge3 = (birthYear) => 2023 - birthYear;
+
+const age3 = calcAge3(1985);
